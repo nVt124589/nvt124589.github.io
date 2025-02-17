6543baca97126a0d00da5c404b0ef7d1ca4f60bd
nVt124589 <tony_do48@yahoo.com>
2025-02-17 10:40:50 -0500

    Create app.js

---
+++ DU-Coursera/SinglePgWebApps_wAngularJS/CdAssignments/test/app.js
@@ -0,0 +1,115 @@
+(function() {
+  'use strict';
+
+  angular.module('NarrowItDownApp', [])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService)
+  .directive('foundItems', FoundItemsDirective)
+  .constant('ApiBasePath', "https://nvt124589.github.io/DU-Coursera/SinglePgWebApps_wAngularJS/CdAssignments");
+
+  var menu_items = [ 'A', 'B', 'C', 'D', 'F', 'L',
+                     'CM', 'CU',
+                     'DK', 'DS',
+                     'FR', 'FY',
+                     'NF', 'NL',
+                     'PF',
+                     'SO', 'SP', 'SR',
+                     'V', 'VG' ];
+
+  function FoundItemsDirective() {
+    var ddo = {
+      templateUrl: '/DU-Coursera/SinglePgWebApps_wAngularJS/CdAssignments/foundItems.html',      
+      scope: {
+        items: '<',
+        title: '@title',
+        message: '@message',
+        onRemove: '&'
+      }
+    };
+
+    return ddo;
+  }
+
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+      var narrowDown = this;
+
+      narrowDown.title = "Menu Items";
+
+      narrowDown.menuSearch = function() {
+        narrowDown.message = "";
+
+        if (isEmptyOrNull(narrowDown.term2Search)) {
+          narrowDown.error = "Search Term cannot be Blank !";
+          narrowDown.nothingFound = "";
+          narrowDown.shown = false;
+        }
+        else {
+          narrowDown.error = "";
+          narrowDown.nothingFound = "* Nothing found !";
+          narrowDown.shown = true;
+        }
+
+        var promise = MenuSearchService.getMatchedMenuItems(narrowDown.term2Search);
+
+        promise
+        .then(function(response) {
+          narrowDown.found = response;
+
+          if (narrowDown.found.length !== 0) narrowDown.message = narrowDown.found.length > 1 ? " items are shown." : " item is shown.";
+          else narrowDown.message = "";
+        })
+        .catch(function(error) { console.log("Error : " + error + " !"); });
+      };
+
+      narrowDown.doNotWantThisOne = function(items, itemIndex) { items.splice(itemIndex, 1); }
+  }
+
+  MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+  function MenuSearchService($http, ApiBasePath) {
+    var service = this;
+
+    service.getMatchedMenuItems = function(searchTerm) {
+      // process result & only keep items that match
+      var foundItems = [];
+
+      return $http({
+          method: "GET",
+          url: (ApiBasePath + "/menu_items.json")
+      })
+      .then(function(menuItems) {
+          /* Loop through all retrieved menu items to pick out the ones
+              whose description matches the searchTerm */
+          for (var i = 0; i < menu_items.length; i++) {
+            var menuItmsVals = menuItems.data[menu_items[i]].menu_items;
+
+            for (var j = 0; j < menuItmsVals.length; j++) {
+              if (!(searchTerm.trim() === "")
+                  && menuItmsVals[j].description.trim().toUpperCase().includes(searchTerm.trim().toUpperCase())) {
+                var item = {
+                  name: menuItmsVals[j].name,
+                  short_name: menuItmsVals[j].short_name,
+                  description: menuItmsVals[j].description
+                };
+
+                foundItems.push(item);
+              }
+            }
+          }
+
+          // return processed items
+          return foundItems;
+      })
+      .catch(function(error) { console.log("Error : " + error + " !"); });
+    }
+  }
+
+  function isEmptyOrNull(value) {
+    if (value === null || value === undefined) { return true; }
+    if (typeof value === 'string' && value.trim() === '') { return true; }
+    if (Array.isArray(value) && value.length === 0) { return true; }
+    if (typeof value === 'object' && Object.keys(value).length === 0) { return true; }
+
+    return false;
+  }
+})();
