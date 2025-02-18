90debc55f5183e0420ae47dee682a1f8d8b90553
Tony Do <tony_do48@yahoo.com>
2025-02-17 19:38:17 -0500

    modify './CdAssignments/module3-solution/' app.js

--- DU-Coursera/SinglePgWebApps_wAngularJS/CdAssignments/module3-solution/app.js
+++ DU-Coursera/SinglePgWebApps_wAngularJS/CdAssignments/module3-solution/app.js
@@ -24,7 +24,7 @@
         title: '@title',
         message: '@message',
         onRemove: '&'
-      },
+      }//,
 
       //controller: FoundItemsDirectiveController,
       //controllerAs: 'narrowDown',
