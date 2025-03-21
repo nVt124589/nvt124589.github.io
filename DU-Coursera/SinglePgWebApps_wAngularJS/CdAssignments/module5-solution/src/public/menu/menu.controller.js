dc2984bb1572373f1bb7d9d28c9e4e497afe5b65
nVt124589 <tony_do48@yahoo.com>
2025-03-20 20:15:29 -0400

    Update menu.controller.js

--- DU-Coursera/SinglePgWebApps_wAngularJS/CdAssignments/module5-solution/src/public/menu/menu.controller.js
+++ DU-Coursera/SinglePgWebApps_wAngularJS/CdAssignments/module5-solution/src/public/menu/menu.controller.js
@@ -1,1 +1,1 @@
-!function(){"use strict";function e(e){var t=this;t.menuItems=e}angular.module("public").controller("MenuItemsController",e),e.$inject=["menuItems"]}();
+!function(){"use strict";function e(e){var n=this;n.menuCategories=e}angular.module("public").controller("MenuController",e),e.$inject=["menuCategories"]}();
