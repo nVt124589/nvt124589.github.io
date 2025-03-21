08db75438c7a9f4a8dbcee80f93b7a71933ba42b
nVt124589 <tony_do48@yahoo.com>
2025-03-20 20:10:36 -0400

    Update common.module.js

--- DU-Coursera/SinglePgWebApps_wAngularJS/CdAssignments/module5-solution/src/common/common.module.js
+++ DU-Coursera/SinglePgWebApps_wAngularJS/CdAssignments/module5-solution/src/common/common.module.js
@@ -1,12 +1,1 @@
-(function() {
-  "use strict";
-
-  angular.module('common', [])
-  .constant('ApiPath', 'https://coursera-jhu-default-rtdb.firebaseio.com')
-  .config(config);
-
-  config.$inject = ['$httpProvider'];
-  function config($httpProvider) {
-    $httpProvider.interceptors.push('loadingHttpInterceptor');
-  }
-})();
+!function(){"use strict";function t(t){t.interceptors.push("loadingHttpInterceptor")}angular.module("common",[]).constant("ApiPath","https://coursera-jhu-default-rtdb.firebaseio.com").config(t),t.$inject=["$httpProvider"]}();
